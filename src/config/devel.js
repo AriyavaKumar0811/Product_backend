77099e5b07eaa4aa26d23a0de5c54e27d4f2e84a
dinesh <dinesh@email.com>
2025-05-04 19:48:09 +0530

    update

--- src/config/devel.js
+++ src/config/devel.js
@@ -1,27 +1,32 @@
 const env = {
 
-    MONGO_URI: '',
-    PORT: 9826,
-    WHITELIST_TFA: '',
+    MONGO_URI: 'NiwbI26euDTh9TVFTHU9ulk2rf3HudWx7wzak9kDgyg=',
+    PORT: 5151,
+    WHITELIST_TFA: 'DPSWY3DPEHPK3DXP',
     SMTP_EMAIL: '',
     SMTP_HOST: '',
     SMTP_PORT: '',
-    SMTP_USER: '',
-    SMTP_PASS: '',
+    SMTP_USER: 'hbs1ET8G3V4HWh4CX6GbkGIKICKIw2W0YFCGT85lENI=',
+    SMTP_PASS: 'fqJZuKxyY55AmGnvdJzxw+phfX+6sfmNw4UjQm4iOu4PMb9XD84KshkEGgUZu5Qv',
     S3_ACCESS_KEY: 'wfC/79F+cX9drfmDLJ2suCEwlNs4tHQxrdb44fUDKyo=',
     S3_SECRET_KEY: 'G1vzqCfnzNVdOUdb0GV1DiNXx3sKFCARH4Z3usLWYuM46efLa++lmoRdsrAdBQQF',
     S3_BUCKET_NAME: 'icoproject',
-    ENDECRYPT_KEY: '#VnfJfbVKPFBut7DcekFK#',
-    ENDECRYPT_IV: '#VnonfVMKPFBmn7DceiK#',
-    GRAEKMLRHKBQEXGSE: '',//5sec token
-    TFA_NAME: ' bot',
-    ADMINPANEL_URL: '',
-    SITE_URL: '',
+    ENDECRYPT_KEY: '#Dvd8CHtAKakSKP#',
+    ENDECRYPT_IV: '#Db9HJZCwCpdg7P#',
+    GRAEKMLRHKBQEXGSE: '',
+    TFA_NAME: ' demo',
+    ADMINPANEL_URL: 'http://localhost:3000/',
+    SITE_URL: 'http://localhost:3000/',
     ALLOWED_ORIGINS: [
-        'http://localhost:5173',
-        'http://localhost:7563',
-        'http://localhost:4201',
-        'http://localhost:4200',
+        'http://localhost:3000',
+        'http://192.168.1.42:3000',
+        'https://192.168.1.42:5151',
+        'http://192.168.78.210:3000',
+        'http://192.168.78.210:5151',
+        'http://192.168.252.210:5151',
+        'http://192.168.252.210:3000'
+
+
 
     ],
 
